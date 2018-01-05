4e129c92cd2ea3271a45f88d8d6b62b3f819e906
syx <syxanash@gmail.com>
2018-01-05 12:27:20 +0000

    improved customization and fixed bug

--- bin/reminder.pl
+++ bin/reminder.pl
@@ -8,9 +8,11 @@ use IPC::System::Simple qw{ capture };
 
 our $VERSION = 0.01;
 
-Readonly my $LIST_LENGTH => 10;
-Readonly my $BOX_WIDTH   => 50;
-Readonly my $LIST_NAME   => 'TODO';
+Readonly my $LIST_LENGTH        => 10;
+Readonly my $BOX_WIDTH          => 50;
+Readonly my $LIST_NAME          => 'TODO';
+Readonly my $SEPARATOR_LINE     => '-';
+Readonly my $VERTICAL_SEPARATOR => '|';
 
 my $cmd_output;
 my $bin_file = '/usr/local/bin/reminders';
@@ -19,15 +21,17 @@ my $bin_file = '/usr/local/bin/reminders';
 chomp( $cmd_output = capture( $bin_file . ' show "' . $LIST_NAME .'"') );
 
 # print top separator line
-print ',', '-' x $BOX_WIDTH, ',', "\n";
+print ',', $SEPARATOR_LINE x $BOX_WIDTH, ',', "\n";
 
 if ( $cmd_output eq q{} ) {
-    my $msg_header = '| [!] ' . $LIST_NAME . ' list is empty!';
-    print $msg_header, q{ } x ( $BOX_WIDTH - length $msg_header ), ' |', "\n";
+    my $msg_header = $VERTICAL_SEPARATOR . ' [!] ' . $LIST_NAME . ' list is empty!';
+    print $msg_header, q{ } x ( $BOX_WIDTH - length $msg_header ), ' ',
+        $VERTICAL_SEPARATOR, "\n";
 }
 else {
-    my $msg_header = '| [?] ' . $LIST_NAME . ' list:';
-    print $msg_header, q{ } x ( $BOX_WIDTH - length $msg_header ), ' |', "\n";
+    my $msg_header = $VERTICAL_SEPARATOR . ' [?] ' . $LIST_NAME . ' list:';
+    print $msg_header, q{ } x ( $BOX_WIDTH - length $msg_header ), ' ',
+        $VERTICAL_SEPARATOR, "\n";
 
     my @lines = split /\n/x, $cmd_output;
     my $is_long = 0;
@@ -41,30 +45,33 @@ else {
         # remove the number which represents the number items from the output
         # returned by reminders tool
         if ( $lines[$i] =~ m{^\d+\s(.*?)$}ix ) {
-            my $msg_item = '| - ' . $1;
+            my $msg_item = $VERTICAL_SEPARATOR . ' - ' . $1;
 
             # when the length is too long add ellipsis
             if ( length($msg_item) > $BOX_WIDTH ) {
                 my $ellipsis = '...';
 
                 $msg_item = substr $msg_item, 0,
-                  -( ( length $msg_item - $BOX_WIDTH ) + length $ellipsis );
+                  -( (length($msg_item) - $BOX_WIDTH) + length($ellipsis) );
                 $msg_item .= $ellipsis;
             }
 
             # print remaining space character until we reach the end of the box
-            print $msg_item, q{ } x ( $BOX_WIDTH - length $msg_item ), ' |', "\n";
+            print $msg_item, q{ } x ( $BOX_WIDTH - length $msg_item ), ' ',
+                $VERTICAL_SEPARATOR, "\n";
         }
     }
 
     # if number of elements in reminder list are too long warn the user
     # of the remaining number of items
     if ($is_long) {
-        my $string_msg = '| ... (' . ( scalar(@lines) - $LIST_LENGTH ) . ' more)';
+        my $string_msg = $VERTICAL_SEPARATOR . ' ... (' .
+            ( scalar(@lines) - $LIST_LENGTH ) . ' more)';
 
-        print $string_msg, q{ } x ( $BOX_WIDTH - length $string_msg ), ' |', "\n";
+        print $string_msg, q{ } x ( $BOX_WIDTH - length $string_msg ), ' ',
+            $VERTICAL_SEPARATOR, "\n";
     }
 }
 
 # print bottom separator line
-print '`', '-' x $BOX_WIDTH, '`', "\n";
+print '`', $SEPARATOR_LINE x $BOX_WIDTH, '`', "\n";
